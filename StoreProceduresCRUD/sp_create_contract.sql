dcba6db6d02e969a576e8d3449c3c88e2c81937d
danielrpg <daniel.fernandezgp@gmail.com>
2018-05-27 20:16:42 -0400

    Procedimientos para el CRUD de Contratos

--- StoreProceduresCRUD/sp_create_contract.sql
+++ StoreProceduresCRUD/sp_create_contract.sql
@@ -1,7 +1,7 @@
 -- Drop User CRUD PROCEDURES
 /******************************************************************************
 **  Table Name: contract
-**  Desc: Table for sp_edit_contract
+**  Desc: Table for sp_create_contract
 ** 
 **  Called by: ssi
 **
@@ -16,32 +16,27 @@
 ** 05/27/2018 Gilmer Daniel Fernandez Pinto   Initial version
 *******************************************************************************/
 
-CREATE PROCEDURE [dbo].[sp_edit_contract](
-    @contract_id INT
-   ,@contract_code VARCHAR(50)
+CREATE PROCEDURE [dbo].[sp_create_contract](
+    @contract_code VARCHAR(50)
    ,@contract_city VARCHAR(100)
    ,@contract_type VARCHAR(100)
    ,@contract_date DATETIME
    ,@contract_salary FLOAT
    ,@contract_description VARCHAR(100)
+   ,@result BIT OUTPUT
 )
 AS 
 SET XACT_ABORT ON;
 SET NOCOUNT ON;
 BEGIN
 
-    UPDATE [dbo].[contracts]
-    SET contract_code       = @contract_code
-        ,contract_city      = @contract_city
-        ,contract_type      = @contract_type
-        ,contract_date      = @contract_date
-        ,contract_salary    = @contract_salary
-        ,contract_description = @contract_description
-        ,updated_on    =  GETDATE()
-    WHERE contract_id = @contract_id;
+    INSERT INTO [dbo].[contracts]
+    (contract_code, contract_city, contract_type, contract_date
+    , contract_salary, contract_description, created_on)
+    VALUES (@contract_code, @contract_city, @contract_type, @contract_date
+           , @contract_salary, @contract_description ,GETDATE());
 
-    SELECT *
-    FROM [dbo].[contracts]
-    WHERE contract_id = @contract_id;
+    SET @result = 1;
 
+    RETURN @result; 
 END
